b74635a4b019e061bbb38afdebed28d0b5045ef8
t-h-omy <thomas.gunter.schneider@gmail.com>
2025-12-03 16:15:03 +0100

    Change SPLASH_SIZE_PREMIUM from 140 to 60

--- data/constants.js
+++ data/constants.js
@@ -147,7 +147,7 @@ export const VISUAL_CONFIG = {
   
   // Size in pixels of splash graphic for premium challenges (Kopfnuss, Zeit-Challenge)
   // Applied in: main.js - generateSplashRaysHtml()
-  SPLASH_SIZE_PREMIUM: 140,
+  SPLASH_SIZE_PREMIUM: 60,
   
   // Size in pixels of splash graphic for Super Challenges
   // Applied in: main.js - generateSplashRaysHtml()
